b41971787116507cad00d6f7560956af36211c30
João Pedro Teles <jpedrorteles@hotmail.com>
2024-09-17 21:47:52 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,115 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+    $('#sendmail').on('click', function(event){
+        event.preventDefault();
+        var nome = $('#nome').val();
+        var email = $('#email').val();
+        var projeto = $('#projeto').val();
+        var mensagem = $('#mensagem').val();
+        var emailCorpo = "mailto:raisedigitalresults@outlook.com?subject=Solicitação de orçamento&body="+
+            "Nome: "+nome+
+            "E-mail: "+email+
+            "Projeto: "+projeto+
+            "Mensagem: "+mensagem;
+        document.location.href = emailCorpo;
+    });
+
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 0, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 50,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Testimonial carousel
+
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 0,
+        nav : true,
+        navText: false,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+     // Fact Counter
+
+     $(document).ready(function(){
+        $('.counter-value').each(function(){
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+            },{
+                duration: 2000,
+                easing: 'easeInQuad',
+                step: function (now){
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    });
+
+
+
+})(jQuery);
+
